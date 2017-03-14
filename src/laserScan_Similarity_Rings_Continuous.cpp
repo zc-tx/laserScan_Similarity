4916f9e534b63375bac4cf94ac49cb2c05e12a73
yh <zjuyh@sina.cn>
2017-03-14 15:55:02 +0800

    start to get more features(intensity&range&ring) to match

--- src/laserScan_Similarity_Rings_Continuous.cpp
+++ src/laserScan_Similarity_Rings_Continuous.cpp
@@ -20,12 +20,23 @@ class scan
 public:
     PM::DataPoints pointCloud;
     int pointCount;
+
+    //Range
     float minRange;
     float maxRange;
     float sectionLength;
     vector<int> sectionCountVector;
     vector<float> sectionRatioVector;
     vector<float> rangeOfPointVector;
+
+    //Intensity Added
+    float minIntensity;
+    float maxIntensity;
+    float sectionLengthOfI;
+    vector<int> sectionCountVectorOfI;
+    vector<float> sectionRatioVectorOfI;
+    vector<float> intensityOfPointVector;
+
     void clear();
 private:
 
@@ -36,11 +47,20 @@ protected:
 void scan::clear()
 {
     pointCount = 0;
+
+    //Range
     minRange = 9999;
     maxRange = 0;
     sectionCountVector.clear();
     sectionRatioVector.clear();
     rangeOfPointVector.clear();
+
+    //Intensity
+    minIntensity = 9999;
+    maxIntensity = 0;
+    sectionCountVectorOfI.clear();
+    sectionRatioVectorOfI.clear();
+    intensityOfPointVector.clear();
 }
 
 class Similarity
@@ -52,72 +72,135 @@ public:
     ~Similarity();
 
     void getEMD1D();
-    void forScan0();
-    void forScan1();
-    void scan0Process();
-    void scan1Process();
+    scan processScan(scan scanInput);
+    float calculateRange(float inputOrdinate);
+    float calculateRangeIn2DPlane(Eigen::Vector2f inputOrdinate);
     float calculateRange(Eigen::Vector3f inputOrdinate);
-    int judgeBucket(float range, float minRange, float sectionLength);
+    int judgeBucket(float range, float minRange, float sectionLength, int sectionNum);
 
     void getRawScan(const sensor_msgs::PointCloud2& cloudMsgIn);
+    DP filterByRange(DP* rawScan);
 
 private:
     ros::Subscriber scanPointCloudSub;
+    ros::Publisher scanPointCloudPub;
+    PM::DataPointsFilters voxelFilters;
 
     scan scan0;
     scan scan1;
 
     ros::NodeHandle& n;
-    const int sectionNum;
-    const bool isLog;
-    const int truncatedStart;
+    const int sectionNumOfR;
+    const int sectionNumOfI;
     const int inputQueueSize;
-    bool first = true;
-    const int ringStart;
-    const int ringEnd;
-    int ringRow;
+    const float rangeThreshold;
+    const bool isVoxelFilter;
+    const float minRange;
+    const float maxRange;
+    const float minIntensity;
+    const float maxIntensity;
 
-    float EMD;
-    vector<vector<float>> scan0RingRatioVector;
-    vector<vector<float>> scan1RingRatioVector;
-    PM::DataPoints *scan0All;
-    PM::DataPoints *scan1All;
+    bool first = true;
 protected:
 
 };
 
 Similarity::Similarity(ros::NodeHandle& n):
     n(n),
-    sectionNum(getParam<int>("sectionNum", 100)),
-    isLog(getParam<bool>("isLog", false)),
-    truncatedStart(getParam<int>("truncatedStart", 0)),
+    sectionNumOfR(getParam<int>("sectionNumOfR", 10)),
     inputQueueSize(getParam<int>("inputQueueSize", 1)),
-    ringStart(getParam<int>("ringStart", 0)),
-    ringEnd(getParam<int>("ringEnd", 0))
+    rangeThreshold(getParam<float>("rangeThreshold", 0)),
+    isVoxelFilter(getParam<bool>("isVoxelFilter", false)),
+    sectionNumOfI(getParam<int>("sectionNumOfI", 10)),
+    minRange(getParam<float>("minRange", 0.0)),
+    maxRange(getParam<float>("maxRange", 100.0)),
+    minIntensity(getParam<float>("minIntensity", 0.0)),
+    maxIntensity(getParam<float>("maxIntensity", 200.0))
 {
+    //load filterConfig  --->  Voxel Grid Filter, thx to Lv
+    string filterConfigName;
+
+    if (ros::param::get("~voxelFilterConfig", filterConfigName))
+    {
+        ifstream ifs(filterConfigName.c_str());
+        if (ifs.good())
+        {
+            voxelFilters = PM::DataPointsFilters(ifs);
+        }
+        else
+        {
+            ROS_ERROR_STREAM("Cannot load voxelFilters config from YAML file " << filterConfigName);
+        }
+    }
+    else
+    {
+        ROS_INFO_STREAM("No voxelFilters config file given, not using these filters");
+    }
+
+    //recieve -> loop
     scanPointCloudSub = n.subscribe("/velodyne_points", inputQueueSize, &Similarity::getRawScan, this);
+    scanPointCloudPub = n.advertise<sensor_msgs::PointCloud2>("after_filtered_points", 2, true);
 }
 
 void Similarity::getRawScan(const sensor_msgs::PointCloud2 &cloudMsgIn)
 {
-    cout<<"---------------------------------"<<endl;
+    cout<<"---------------------------------------"<<endl;
+
+    DP* rawScan = new DP(PointMatcher_ros::rosMsgToPointMatcherCloud<float>(cloudMsgIn));
+
+    //Apply Voxel Filter
+    if(this->isVoxelFilter)
+    {
+        voxelFilters.apply(*rawScan);
+    }
+
     if(first)
     {
-        scan0All = new DP(PointMatcher_ros::rosMsgToPointMatcherCloud<float>(cloudMsgIn));
-        this->forScan0();
-        scan1All = new DP(PointMatcher_ros::rosMsgToPointMatcherCloud<float>(cloudMsgIn));
-        this->forScan1();
+        scan0.pointCloud = this->filterByRange(rawScan);
+
+        scan0 = this->processScan(scan0);
+
+        scan1.pointCloud = this->filterByRange(rawScan);
+
+        scan1 = this->processScan(scan1);
 
         first = false;
     }
     else
     {
-        scan1All = new DP(PointMatcher_ros::rosMsgToPointMatcherCloud<float>(cloudMsgIn));
-        this->forScan1();
+        scan1.pointCloud = this->filterByRange(rawScan);
+
+        scan1 = this->processScan(scan1);
     }
 
     this->getEMD1D();
+
+    //Pub
+    scanPointCloudPub.publish(PointMatcher_ros::pointMatcherCloudToRosMsg<float>(scan1.pointCloud, "velodyne", ros::Time::now()));
+
+    //Clear Scan1
     scan1.clear();
+}
+
+Similarity::DP Similarity::filterByRange(DP *rawScan)
+{
+    DP tempScan;
+    int count = 0;
+    tempScan = rawScan->createSimilarEmpty();
+    for(int i = 0; i < rawScan->features.cols(); i++)
+    {
+        float rangeOfPoint = calculateRangeIn2DPlane(rawScan->features.col(i).head(2));
+        if(rangeOfPoint > this->rangeThreshold)
+        {
+            tempScan.setColFrom(count, *rawScan, i);
+            count++;
+        }
+    }
+    tempScan.conservativeResize(count);
+
+    cout<<"Range filtered:  "<<rawScan->features.cols()-tempScan.features.cols()<<endl;
+
+    return tempScan;
 }
 
 Similarity::~Similarity()
@@ -125,211 +208,176 @@ Similarity::~Similarity()
 
 }
 
-void Similarity::forScan0()
+scan Similarity::processScan(scan scanInput)
 {
-    //Process by Ring
-    int ringRow = scan0All->getDescriptorStartingRow("ring");
+    scanInput.pointCount = scanInput.pointCloud.features.cols();
 
-    for(int i = ringStart; i <= ringEnd; i++)
+    ///scanInput Processing by Range
     {
-        scan0.pointCloud = scan0All->createSimilarEmpty();
-        int count = 0;
-        for(int j = 0; j < scan0All->features.cols(); j++)
+        scanInput.maxRange = -1;
+        scanInput.minRange = 9999;
+        for(int i = 0; i < scanInput.pointCount; i++)
         {
-            if(scan0All->descriptors(ringRow, j) == i)
-            {
-                scan0.pointCloud.setColFrom(count, *scan0All, j);
-                count++;
-            }
-        }
-        scan0.pointCloud.conservativeResize(count);
+            float rangeOfPoint = calculateRange(scanInput.pointCloud.features.col(i).head(3));   //the range
+//            float rangeOfPoint = calculateRange(scanInput.pointCloud.features(2, i)); //Z-axis range only
+            scanInput.rangeOfPointVector.push_back(rangeOfPoint);
 
-        this->scan0Process();
+            ///Thanks to Tang Li's advice
+//            if(rangeOfPoint > scanInput.maxRange)
+//                scanInput.maxRange = rangeOfPoint;
+//            if(rangeOfPoint < scanInput.minRange)
+//                scanInput.minRange = rangeOfPoint;
 
-    }
-}
-
-void Similarity::scan0Process()
-{
-
-    //Scan0 Pretreated
-    scan0.pointCount = scan0.pointCloud.features.cols();
-    scan0.maxRange = 0;
-    scan0.minRange = 9999;
-    for(int i = 0; i < scan0.pointCount; i++)
-    {
-        float rangeOfPoint = calculateRange(scan0.pointCloud.features.col(i).head(3));
-        scan0.rangeOfPointVector.push_back(rangeOfPoint);
-        if(rangeOfPoint > scan0.maxRange)
-            scan0.maxRange = rangeOfPoint;
-        if(rangeOfPoint < scan0.minRange)
-            scan0.minRange = rangeOfPoint;
-    }
-    scan0.sectionLength = (scan0.maxRange - scan0.minRange) / sectionNum;
-    for(int i = 0; i < sectionNum; i++)
-    {
-        scan0.sectionCountVector.push_back(0);
-    }
-//    cout<<"in Scan0 maxRange:  "<<scan0.maxRange
-//       <<"  minRange:  "<<scan0.minRange
-//       <<"  count:  "<<scan0.pointCount
-//      <<"  sectionLength:  "<<scan0.sectionLength<<endl;
-
-    ///Scan0
-    //Count
-    for(int i = 0; i < scan0.pointCount; i++)
-    {
-        int index = this->judgeBucket(scan0.rangeOfPointVector.at(i), scan0.minRange, scan0.sectionLength);
-        scan0.sectionCountVector.at(index)++;
-    }
-    //Ratio
-    for(int i = 0; i < sectionNum; i++)
-    {
-        scan0.sectionRatioVector.push_back((float)scan0.sectionCountVector.at(i) / (float)scan0.pointCount);
-    }
-//    //Check
-//    float sumRatio = 0;
-//    int sumCount = 0;
-//    for(int i = 0; i < sectionNum; i++)
-//    {
-//        sumRatio += scan0.sectionRatioVector.at(i);
-//        sumCount += scan0.sectionCountVector.at(i);
-//    }
-//    cout<<"sum0: "<<sumRatio<<"  "<<sumCount<<endl;
-
-    scan0RingRatioVector.push_back(scan0.sectionRatioVector);
-}
-
-void Similarity::forScan1()
-{
-    scan1RingRatioVector.clear();
-
-    //Process by Ring
-    int ringRow = scan1All->getDescriptorStartingRow("ring");
-
-    for(int i = ringStart; i <= ringEnd; i++)
-    {
-        scan1.pointCloud = scan1All->createSimilarEmpty();
-        int count = 0;
-        for(int j = 0; j < scan1All->features.cols(); j++)
-        {
-            if(scan1All->descriptors(ringRow, j) == i)
-            {
-                scan1.pointCloud.setColFrom(count, *scan1All, j);
-                count++;
-            }
-        }
-        scan1.pointCloud.conservativeResize(count);
-
-        this->scan1Process();
-
-    }
-
-}
-
-void Similarity::scan1Process()
-{
-
-    //Scan1 PreTreated
-    scan1.pointCount = scan1.pointCloud.features.cols();
-    scan1.maxRange = 0;
-    scan1.minRange = 9999;
-    for(int i = 0; i < scan1.pointCount; i++)
-    {
-        float rangeOfPoint = calculateRange(scan1.pointCloud.features.col(i).head(3));
-        scan1.rangeOfPointVector.push_back(rangeOfPoint);
-        if(rangeOfPoint > scan1.maxRange)
-            scan1.maxRange = rangeOfPoint;
-        if(rangeOfPoint < scan1.minRange)
-            scan1.minRange = rangeOfPoint;
-    }
-    scan1.sectionLength = (scan1.maxRange - scan1.minRange) / sectionNum;
-    for(int i = 0; i < sectionNum; i++)
-    {
-        scan1.sectionCountVector.push_back(0);
-    }
-//    cout<<"in Scan1 maxRange:  "<<scan1.maxRange
-//       <<"  minRange:  "<<scan1.minRange
-//        <<"  count:  "<<scan1.pointCount
-//       <<"  sectionLength:  "<<scan1.sectionLength<<endl;
-
-    ///Scan1
-    //Count
-    for(int i = 0; i < scan1.pointCount; i++)
-    {
-        int index = this->judgeBucket(scan1.rangeOfPointVector.at(i), scan1.minRange, scan1.sectionLength);
-        scan1.sectionCountVector.at(index)++;
-    }
-    //Ratio
-    for(int i = 0; i < sectionNum; i++)
-    {
-        scan1.sectionRatioVector.push_back((float)scan1.sectionCountVector.at(i) / (float)scan1.pointCount);
-    }
-//    //Check
-//    sumRatio = 0;
-//    sumCount = 0;
-//    for(int i = 0; i < sectionNum; i++)
-//    {
-//        sumRatio += scan1.sectionRatioVector.at(i);
-//        sumCount += scan1.sectionCountVector.at(i);
-//    }
-//    cout<<"sum1: "<<sumRatio<<"  "<<sumCount<<endl;
-
-    scan1RingRatioVector.push_back(scan1.sectionRatioVector);
-
-    if(isLog)
-    {
-        cout<<"Log two vectors"<<endl;
-        ofstream recordVector0, recordVector1;
-        stringstream recordName0, recordName1;
-
-        //ugly code for test
-        recordName0 << "/home/yh/0.txt";
-        recordName1 << "/home/yh/1.txt";
-
-        recordVector0.open(recordName0.str());
-        recordVector1.open(recordName1.str());
-
-        for(int i = 0; i < sectionNum; i++)
-        {
-            recordVector0 <<std::fixed<< scan0.sectionRatioVector.at(i) <<endl;
-            recordVector1 <<std::fixed<< scan1.sectionRatioVector.at(i) <<endl;
         }
 
-        recordVector0.close();
-        recordVector1.close();
+        scanInput.minRange = this->minRange;
+        scanInput.maxRange = this->maxRange;
+        scanInput.sectionLength = (scanInput.maxRange - scanInput.minRange) / sectionNumOfR;
+
+        for(int i = 0; i < sectionNumOfR; i++)
+        {
+            scanInput.sectionCountVector.push_back(0);
+        }
+
+
+        ///scanInput statistics
+        //Count
+        for(int i = 0; i < scanInput.pointCount; i++)
+        {
+            int index = this->judgeBucket(scanInput.rangeOfPointVector.at(i),
+                                          scanInput.minRange,
+                                          scanInput.sectionLength,
+                                          this->sectionNumOfR);
+            scanInput.sectionCountVector.at(index)++;
+        }
+        //Ratio
+        for(int i = 0; i < sectionNumOfR; i++)
+        {
+            scanInput.sectionRatioVector.push_back((float)scanInput.sectionCountVector.at(i) / (float)scanInput.pointCount);
+        }
     }
 
+    ///scanInput Processing by Intensity
+    {
+        int intensityRow = scanInput.pointCloud.getDescriptorStartingRow("intensity");
+        scanInput.maxIntensity = -1;
+        scanInput.minIntensity = 9999;
+
+        for(int i = 0; i < scanInput.pointCount; i++)
+        {
+            float intensityOfPoint = scanInput.pointCloud.descriptors(intensityRow, i);
+            scanInput.intensityOfPointVector.push_back(intensityOfPoint);
+
+//            if(intensityOfPoint > scanInput.maxIntensity)
+//                scanInput.maxIntensity = intensityOfPoint;
+//            if(intensityOfPoint < scanInput.minIntensity)
+//                scanInput.minIntensity = intensityOfPoint;
+
+        }
+
+//        cout<<"maxIntensity:  "<<scanInput.maxIntensity< <"  minIntensity:  "<<scanInput.minIntensity<<endl;
+        scanInput.minIntensity = this->minIntensity;
+        scanInput.maxIntensity = this->maxIntensity;
+        scanInput.sectionLengthOfI = (scanInput.maxIntensity - scanInput.minIntensity) / sectionNumOfI;
+
+        for(int i = 0; i < sectionNumOfI; i++)
+        {
+            scanInput.sectionCountVectorOfI.push_back(0);
+        }
+
+        ///scanInput statistics
+        //Count
+        for(int i = 0; i < scanInput.pointCount; i++)
+        {
+            int index = this->judgeBucket(scanInput.intensityOfPointVector.at(i),
+                                          scanInput.minIntensity,
+                                          scanInput.sectionLengthOfI,
+                                          this->sectionNumOfI);
+            scanInput.sectionCountVectorOfI.at(index)++;
+        }
+        //Ratio
+        for(int i = 0; i < sectionNumOfI; i++)
+        {
+            scanInput.sectionRatioVectorOfI.push_back((float)scanInput.sectionCountVectorOfI.at(i) / (float)scanInput.pointCount);
+        }
+    }
+
+    return scanInput;
 }
 
 void Similarity::getEMD1D()
 {
-    EMD = 0;
-    for(int m = 0; m <= ringEnd - ringStart; m++)
+    float EMDRange = 0;
+    for(int i = 0; i < sectionNumOfR; i++)
     {
-        for(int i = this->truncatedStart; i < sectionNum; i++)
+        for(int j = 0; j <= i; j++)
         {
-            for(int j = 0; j <= i; j++)
-            {
-                EMD += abs(scan0RingRatioVector[m][j] - scan1RingRatioVector[m][j]);
-            }
+            EMDRange += abs(scan0.sectionRatioVector.at(j) - scan1.sectionRatioVector.at(j));
         }
     }
-    ///?
-    EMD /= (sectionNum * (ringEnd - ringStart + 1));
-    ///?
-    cout<<"EMD Distance:  "<<EMD<<endl;
+    EMDRange /= sectionNumOfR;
+    cout<<"EMD Range Distance:  "<<EMDRange<<endl;
 
-    //save in the EMD.txt
+    float EMDIntensity = 0;
+    for(int i = 0; i < sectionNumOfI; i++)
+    {
+        for(int j = 0; j <= i; j++)
+        {
+            EMDIntensity += abs(scan0.sectionRatioVectorOfI.at(j) - scan1.sectionRatioVectorOfI.at(j));
+        }
+    }
+    EMDIntensity /= sectionNumOfI;
+    cout<<"EMD Intensity Distance:  "<<EMDIntensity<<endl;
+
+    //save the 1D EMD Distance in the EMD.txt
     if(1)
     {
-        ofstream recordEMD;
-        stringstream ssEMD;
+        ofstream recordEMDRange;
+        stringstream ssEMDRange;
 
-        ssEMD <<"/home/yh/EMD1D.txt";
-        recordEMD.open(ssEMD.str(), ios::app);
-        recordEMD << std::fixed << EMD << endl;
+        ssEMDRange <<"/home/yh/EMDRange.txt";
+        recordEMDRange.open(ssEMDRange.str(), ios::app);
+        recordEMDRange << std::fixed << EMDRange << endl;
+
+        ofstream recordEMDIntensity;
+        stringstream ssEMDIntensity;
+
+        ssEMDIntensity <<"/home/yh/EMDIntensity.txt";
+        recordEMDIntensity.open(ssEMDIntensity.str(), ios::app);
+        recordEMDIntensity << std::fixed << EMDIntensity << endl;
     }
+
+    //save the origin data to draw the Similarity Matrix
+    if(1)
+    {
+        ofstream recordBinRange;
+        stringstream ssBinRange;
+
+        ssBinRange << "/home/yh/BinRange.txt";
+        recordBinRange.open(ssBinRange.str(), ios::app);
+
+        for(int i = 0; i < sectionNumOfR; i++)
+        {
+            recordBinRange << std::fixed << scan1.sectionRatioVector.at(i)<<" ";
+        }
+
+        recordBinRange <<endl;
+
+        ofstream recordBinIntensity;
+        stringstream ssBinIntensity;
+
+        ssBinIntensity << "/home/yh/BinIntensity.txt";
+        recordBinIntensity.open(ssBinIntensity.str(), ios::app);
+
+        for(int i = 0; i < sectionNumOfI; i++)
+        {
+            recordBinIntensity << std::fixed << scan1.sectionRatioVectorOfI.at(i)<<" ";
+        }
+
+        recordBinIntensity <<endl;
+
+    }
+
 }
 
 float Similarity::calculateRange(Eigen::Vector3f inputOrdinate)
@@ -337,7 +385,17 @@ float Similarity::calculateRange(Eigen::Vector3f inputOrdinate)
     return pow(pow(inputOrdinate(0), 2) + pow(inputOrdinate(1), 2) + pow(inputOrdinate(2), 2), 0.5);
 }
 
-int Similarity::judgeBucket(float range, float minRange, float sectionLength)
+float Similarity::calculateRangeIn2DPlane(Eigen::Vector2f inputOrdinate)
+{
+    return pow(pow(inputOrdinate(0), 2) + pow(inputOrdinate(1), 2), 0.5);
+}
+
+float Similarity::calculateRange(float inputOrdinate)
+{
+    return inputOrdinate;
+}
+
+int Similarity::judgeBucket(float range, float minRange, float sectionLength, int sectionNum)
 {
     for(int i = 0; i < sectionNum; i++)
     {
