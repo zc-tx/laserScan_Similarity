9eb0522f773ac46c65e01ebb4e6afa013b5d192c
yh <zjuyh@sina.cn>
2017-05-02 19:09:23 +0800

    ”m“

---
+++ src/saveImagesName.m
@@ -0,0 +1,39 @@
+function [  ] = saveImagesName( set, trueIndex, falseIndex )
+%SAVEIMAGESNAME Summary of this function goes here
+%   Detailed explanation goes here
+    
+    dir = '/home/yh/kitti_test/imagesName/';
+    setName = num2str(set);
+    
+    fileNameTrue = [dir, setName, 'True.txt'];
+    fileNameFalse = [dir, setName, 'False.txt'];
+    
+    fidTrue = fopen(fileNameTrue, 'w+');
+    fidFalse = fopen(fileNameFalse, 'w+');
+    
+    lenTrue = length(trueIndex);
+    lenFalse = length(falseIndex);
+    
+    for i = 1 : 1 : lenTrue
+        name1 = sprintf('%04d', trueIndex(i, 1));
+        name2 = sprintf('%04d', trueIndex(i, 2));
+        imageName1 = ['/home/yh/kitti_test/images/', setName, name1, '.jpg   ']; 
+        imageName2 = ['/home/yh/kitti_test/images/', setName, name2, '.jpg']; 
+        fprintf(fidTrue, '%s', imageName1);
+        fprintf(fidTrue, '%s\n', imageName2);       
+    end
+    
+    for j = 1 : 1 : lenFalse
+        name1 = sprintf('%04d', falseIndex(i, 1));
+        name2 = sprintf('%04d', falseIndex(i, 2));
+        imageName1 = ['/home/yh/kitti_test/images/', setName, name1, '.jpg   ']; 
+        imageName2 = ['/home/yh/kitti_test/images/', setName, name2, '.jpg']; 
+        fprintf(fidFalse, '%s', imageName1);
+        fprintf(fidFalse, '%s\n', imageName2);   
+    end
+    
+    
+
+
+end
+
--- src/saveImagesRGB.m
+++ src/saveImagesRGB.m
@@ -3,16 +3,19 @@ function [  ] = saveImagesRGB( set, imagesRange, imagesIntensity )
 %   Detailed explanation goes here
     
     len = length(imagesRange);
-    RGB = zeros(64, 80, 3);
-    dir = '/home/yh/images/';
+    RGB = zeros(64, 80, 2);
+    dir = '/home/yh/kitti_test/images/';
     
     for i = 1 : 1 : len
         i
         for m = 1 : 1 : 64
            for n = 1 : 1 : 80
-              RGB(m, n, 1) = uint8(imagesRange{i}(m, n)*255);
-              RGB(m, n, 2) = uint8(imagesIntensity{i}(m, n)*255);
-              RGB(m, n, 3) = uint8(0); 
+              RGB(m, n, 1) = uint8(imagesRange{i}(m, n)*256);
+              RGB(m, n, 2) = uint8(imagesIntensity{i}(m, n)*256);
+              RGB(m, n, 3) = uint8(0);
+%               RGB(m, n, 1)
+%               RGB(m, n, 2)
+%               RGB(m, n, 3)
            end
         end
         
